0d9ea93068edae3abc769446a983b6d8af39ca61
Eugene Zharov <deeech88@gmail.com>
2016-10-26 10:26:47 +0300

    found some bugs

--- .gemini.js
+++ .gemini.js
@@ -2,7 +2,7 @@ module.exports = {
   rootUrl: 'http://127.0.0.1:8080',
   gridUrl: 'http://127.0.0.1:4444/wd/hub',
   compositeImage: true,
-  // windowSize: '1600x1080',
+  // windowSize: '1024x768',
   browsers: {
     chrome: {
       desiredCapabilities: {
--- gemini/test.js
+++ gemini/test.js
@@ -1,5 +1,20 @@
 gemini.suite('minifiers-test', (suite) => {
-  suite.setUrl('/')
-    .capture('plain')
-    .setCaptureElements('html')
+  suite
+    .setUrl('/')
+      .capture('no min')
+      .setCaptureElements('html')
+    .setUrl('/clean-css_index.html')
+      .capture('min')
+      .setCaptureElements('html')
+
+  // gemini.suite('no min', function(child) {
+  //   child.setUrl('/')
+  //     .capture('no min')
+  //     .setCaptureElements('html')
+  // });
+  // gemini.suite('min', function(child) {
+  //   child.setUrl('/clean-css_index.html')
+  //     .capture('min')
+  //     .setCaptureElements('html')
+  // });
 });

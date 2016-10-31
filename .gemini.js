12d64dc0546bfe3650facc4397928cda69bc415c
Eugene Zharov <deeech88@gmail.com>
2016-10-31 05:53:37 +0300

    better results

--- .gemini.js
+++ .gemini.js
@@ -1,7 +1,7 @@
 module.exports = {
   rootUrl: 'http://127.0.0.1:8080',
   gridUrl: 'http://127.0.0.1:4444/wd/hub',
-  windowSize: '1920x768',
+  windowSize: '1920x1080',
   sessionQuitTimeout: 10000,
   httpTimeout: 10000,
   compositeImage: true,

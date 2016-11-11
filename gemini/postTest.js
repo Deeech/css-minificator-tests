2bcc81e14ceae924c1e4b6cc9fbaf4b754414b61
Eugene Zharov <deeech88@gmail.com>
2016-11-11 16:57:35 +0300

    Updates

--- gemini/postTest.js
+++ gemini/postTest.js
@@ -1,14 +1,6 @@
 
 gemini.suite('nodejs.org', (suite) => {
 
-  gemini.suite('no minified', function(child) {
-    child.setUrl('/nodejs.org/index.html')
-      .capture('no minified', function(actions, find) {
-        actions.wait(2000);
-      })
-      .setCaptureElements('html')
-  });
-
   gemini.suite('clean-css', function(child) {
     child.setUrl('/nodejs.org/clean-css_index.html')
       .capture('clean-css', function(actions, find) {
@@ -133,14 +125,6 @@ gemini.suite('nodejs.org', (suite) => {
 
 gemini.suite('wordpress.org', (suite) => {
 
-  gemini.suite('no minified', function(child) {
-    child.setUrl('/wordpress.org/index.html')
-      .capture('no minified', function(actions, find) {
-        actions.wait(2000);
-      })
-      .setCaptureElements('html')
-  });
-
   gemini.suite('clean-css', function(child) {
     child.setUrl('/wordpress.org/clean-css_index.html')
       .capture('clean-css', function(actions, find) {
@@ -263,15 +247,131 @@ gemini.suite('wordpress.org', (suite) => {
 
 });
 
-gemini.suite('github.com', (suite) => {
+gemini.suite('Simpsons', (suite) => {
 
-  gemini.suite('no minified', function(child) {
-    child.setUrl('/github.com/index.html')
-      .capture('no minified', function(actions, find) {
+  gemini.suite('clean-css', function(child) {
+    child.setUrl('/Simpsons/clean-css_index.html')
+      .capture('clean-css', function(actions, find) {
         actions.wait(2000);
       })
-      .setCaptureElements('html')
+      .setCaptureElements('#wrap')
   });
+
+  gemini.suite('clean-css_advanced_off', function(child) {
+    child.setUrl('/Simpsons/clean-css_advanced_off_index.html')
+      .capture('clean-css_advanced_off', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('crass', function(child) {
+    child.setUrl('/Simpsons/crass_index.html')
+      .capture('crass', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('crass_o1_off', function(child) {
+    child.setUrl('/Simpsons/crass_o1_off_index.html')
+      .capture('crass_o1_off', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('css-condense', function(child) {
+    child.setUrl('/Simpsons/css-condense_index.html')
+      .capture('css-condense', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('cssnano', function(child) {
+    child.setUrl('/Simpsons/cssnano_index.html')
+      .capture('cssnano', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('cssnano (safe)', function(child) {
+    child.setUrl('/Simpsons/cssnano (safe)_index.html')
+      .capture('cssnano (safe)', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('csso', function(child) {
+    child.setUrl('/Simpsons/csso_index.html')
+      .capture('csso', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('csso_restructure_off', function(child) {
+    child.setUrl('/Simpsons/csso_restructure_off_index.html')
+      .capture('csso_restructure_off', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('cssshrink', function(child) {
+    child.setUrl('/Simpsons/cssshrink_index.html')
+      .capture('cssshrink', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('csswring', function(child) {
+    child.setUrl('/Simpsons/csswring_index.html')
+      .capture('csswring', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('more-css', function(child) {
+    child.setUrl('/Simpsons/more-css_index.html')
+      .capture('more-css', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('ncss', function(child) {
+    child.setUrl('/Simpsons/ncss_index.html')
+      .capture('ncss', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('sqwish', function(child) {
+    child.setUrl('/Simpsons/sqwish_index.html')
+      .capture('sqwish', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+  gemini.suite('ycssmin', function(child) {
+    child.setUrl('/Simpsons/ycssmin_index.html')
+      .capture('ycssmin', function(actions, find) {
+        actions.wait(2000);
+      })
+      .setCaptureElements('#wrap')
+  });
+
+});
+
+gemini.suite('github.com', (suite) => {
 
   gemini.suite('clean-css', function(child) {
     child.setUrl('/github.com/clean-css_index.html')
